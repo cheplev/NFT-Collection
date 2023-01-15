43acc1bf770b780158ae5414bdb5b9618ae693d4
chep <vcheplev42@gmail.com>
2023-01-15 17:08:00 +0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xadFe6FED8A9986814ea209ED72DDe915F860ce53";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
