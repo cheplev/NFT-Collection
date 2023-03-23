324b25fa309e440fa170179b58aab9c05f4055e8
chep <vcheplev42@gmail.com>
2023-03-23 18:26:58 +0400

    contact address change

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -568,4 +568,4 @@ export const abi = [
     "type": "receive"
   }
 ];
-export const NFT_CONTRACT_ADDRESS = "0xB5f8D4f4e32D01dAD93932747d9F03263e444EfB"
+export const NFT_CONTRACT_ADDRESS = "0x38119e45cBa4bbdd4a71d8ffD029f7EF7F4FBAdA"
